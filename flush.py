7ae61b6da9ad0824044264a3bd991b16c0ed7d8d
Rafał Gajdulewicz <rafal@rafalubuntu.(none)>
2012-09-11 22:28:14 +0200

    Pickle in batches

--- flush.py
+++ flush.py
@@ -1,5 +1,7 @@
 import os
-import pickle
+import cPickle
+import thread
+import copy
 from flask import Flask,send_from_directory
 from base62_converter import saturate, dehydrate
 app = Flask(__name__)
@@ -17,7 +19,6 @@ def favicon():
 def get_url(uid):
 	if uid in shortened_urls:
 		url_visits[uid] += 1
-		pickle.dump(url_visits,open('url_visits.db','w'))
 		return shortened_urls[uid]
 	return "No such url %s !" % uid
 
@@ -26,10 +27,10 @@ def shorten(url):
 	global cnt
 	cnt += 1
 	uid = dehydrate(cnt)
+	if cnt%100==0:
+		thread.start_new_thread(dump_to_disk, ())
 	shortened_urls[uid]=url
 	url_visits[uid]=0
-	pickle.dump(shortened_urls,open('shortened_urls.db','w'))
-	pickle.dump(url_visits,open('url_visits.db','w'))
 	return "Shortened to %s"%uid
 
 @app.route("/info/<uid>")
@@ -38,10 +39,18 @@ def info(uid):
 		return "Url: %s visited %s times" %(shortened_urls[uid],url_visits[uid])
 	return "No such url %s !" % uid
 
+
+def dump_to_disk():
+	print "Dumping"
+	_shortened_urls = copy.copy(shortened_urls)
+	_url_visits = copy.copy(url_visits)
+	cPickle.dump(_shortened_urls,open('shortened_urls.db','w'))
+	cPickle.dump(_url_visits	,open('url_visits.db','w'))
+
 if __name__ == "__main__":
 	if os.path.exists('shortened_urls.db'):
-		shortened_urls = pickle.load(open('shortened_urls.db','r'))
+		shortened_urls = cPickle.load(open('shortened_urls.db','r'))
 		cnt = len(shortened_urls)
 	if os.path.exists('url_visits.db'):
-		url_visits = pickle.load(open('url_visits.db','r'))
+		url_visits = cPickle.load(open('url_visits.db','r'))
 	app.run( debug=True)
