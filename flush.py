403c17f4dcda26f353f0a4d7a13235b2c7f78dbe
Rafal Gajdulewicz <rgajdulewicz@apreel.com>
2012-09-12 14:02:41 +0200

    Use redis to persist data

--- flush.py
+++ flush.py
@@ -4,56 +4,41 @@ import thread
 import copy
 from flask import Flask, send_from_directory
 from base62_converter import saturate, dehydrate
+from store import redis
 app = Flask(__name__)
-
-cnt = 0
-shortened_urls = {}
-url_visits = {}
 
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route("/<uid>")
 def get_url(uid):
-    if uid in shortened_urls:
-        url_visits[uid] += 1
-        return shortened_urls[uid]
+    url = redis.get('u:%s' % uid)
+    if url:
+        redis.incr('v:%s' % uid)
+        return url
     return "No such url %s !" % uid
 
 
 @app.route("/shorten/<url>")
 def shorten(url):
-    global cnt
-    cnt += 1
+    cnt = redis.incr('count')
     uid = dehydrate(cnt)
-    if cnt % 100 == 0:
-        thread.start_new_thread(dump_to_disk, ())
-    shortened_urls[uid] = url
-    url_visits[uid] = 0
+    redis.set('u:%s' % uid, url)
     return "Shortened to %s" % uid
 
 
 @app.route("/info/<uid>")
 def info(uid):
-    if uid in shortened_urls:
-        return "Url: %s visited %s times" % (shortened_urls[uid], url_visits[uid])
+    url = redis.get('u:%s' % uid)
+    visits = redis.get('v:%s' % uid)
+    if url and visits:
+        return "Url: %s visited %s times" % (url, visits)
     return "No such url %s !" % uid
 
-def dump_to_disk():
-    print "Dumping"
-    _shortened_urls = copy.copy(shortened_urls)
-    _url_visits = copy.copy(url_visits)
-    cPickle.dump(_shortened_urls, open('shortened_urls.db', 'w'))
-    cPickle.dump(_url_visits, open('url_visits.db', 'w'))
-
 if __name__ == "__main__":
-    if os.path.exists('shortened_urls.db'):
-        shortened_urls = cPickle.load(open('shortened_urls.db', 'r'))
-        cnt = len(shortened_urls)
-    if os.path.exists('url_visits.db'):
-        url_visits = cPickle.load(open('url_visits.db', 'r'))
     app.run(debug=True)
