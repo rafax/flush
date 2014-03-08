22358fc7c31b04a1c84b14288d6dc8eaaf773949
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2014-03-08 12:55:38 +0100

    Add test files

---
+++ test.sh
@@ -0,0 +1,8 @@
+redis-cli flushdb
+redis-cli SET url:1 google.com
+redis-cli SET url:2 apreel.com
+redis-cli SET url:3 amazon.com
+redis-cli SET url:4 gajdulewicz.com
+redis-cli SET url:5 gazeta.pl
+redis-cli SET stats:count 5
+siege -t 30s -f test_urls.txt -q -d 1s -c 200
